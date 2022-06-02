13bd29430ed018e07d08756d726403ec5660047d
Mohammed Sohail <sohailsameja@gmail.com>
2022-06-02 12:08:07 +0300

    add: pin status page

--- pages/locked-pins.vue
+++ pages/locked-pins.vue
@@ -1,6 +1,19 @@
 <template>
   <v-container>
-    <h1>locked pins</h1>
+    <v-row justify="center">
+      <v-col cols="12" lg="7">
+        <v-card elevation="0">
+          <v-card-title>Pin Status</v-card-title>
+          <v-data-table
+            :headers="headers"
+            :items="pinStatus"
+            class="elevation-0"
+          >
+            <template #no-data> No results </template>
+          </v-data-table>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -8,5 +21,33 @@
 export default {
   name: 'LockedPinsPage',
   middleware: 'auth',
+  async asyncData({ $axios }) {
+    const pinStatus = await $axios.get(`/admin/pin-status`)
+
+    return {
+      pinStatus: pinStatus.data,
+    }
+  },
+  data() {
+    return {
+      pinStatus: [],
+      headers: [
+        {
+          text: 'Phone Number',
+          value: 'phone_number',
+        },
+        {
+          text: 'Failed Pin Attempts',
+          sortable: true,
+          value: 'failed_pin_attempts',
+        },
+        {
+          text: 'Account Status',
+          sortable: true,
+          value: 'account_status',
+        },
+      ],
+    }
+  },
 }
 </script>
